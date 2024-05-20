644f04988162c221c0f1920d15a0f4f9010be186
alefcaldas <128673069+alefcaldas@users.noreply.github.com>
2024-05-20 11:29:20 -0300

    Update BancoJavaDIO.java

--- BancoJavaDIO.java
+++ BancoJavaDIO.java
@@ -9,15 +9,15 @@ public class BancoJavaDIO {
 
         System.out.println("digite seu nome");
         nomeDoCliente = sc.nextLine();
-        System.out.println("por favor digite uma senha");
-        senha = sc.nextLine();
+            System.out.println("por favor digite uma senha");
+            senha = sc.nextLine();
 
         System.out.println("Nós trabalhamos com um metodo em que você escolhe a conta e agencia então");
 
         System.out.println("digite aqui a agencia");
         agencia1 = sc.nextInt();
-        System.out.println("digite aqui a sua conta");
-        conta1 = sc.nextInt();
+            System.out.println("digite aqui a sua conta");
+            conta1 = sc.nextInt();
 
         System.out.println("muito bem " + nomeDoCliente + " seu acesso e conta foram criados, agora que você é um " +
                 "cliente do nosso banco o que gostaria de fazer agora?");
@@ -29,21 +29,26 @@ public class BancoJavaDIO {
         if (acao == 1) {
             System.out.println("muito bem como você é novo por aqui então seu saldo ainda é 0");
         }
-        if (acao == 2) {
-            System.out.println("muto bem como aqui você ainda é novo por aqui sua conta ainda não tem movimentações");
-        }
-        if (acao == 3) {
-            System.out.println("por favor digite a conta e agencia que deseja transferir o dinheiro");
+            if (acao == 2) {
+                System.out.println("muto bem como aqui você ainda é novo por aqui sua conta ainda não tem movimentações");
+            }
+                if (acao == 3) {
+                    System.out.println("agencia que vai receber o dinheiro");
+                    agencia2 = sc.nextInt();
+
+                    System.out.println("conta que vai receber o dinheiro");
+                    conta2 = sc.nextInt();
+
+                    saldo = 0;
+
+                    System.out.println("so um momento que estamos processando a transferencia" +
+                            " parece que seu saldo é 0 então a transferencia não pôde ser realizada");
         }
 
         saldo = 0;
 
-        System.out.println("agencia que vai receber o dinheiro");
-        agencia2 = sc.nextInt();
-        System.out.println("conta que vai receber o dinheiro");
-        conta2 = sc.nextInt();
-
-        if (saldo == 0) {
+    }
+}
             System.out.println("so um momento que estamos processando a transferencia" +
                     " parece que seu saldo é 0 então a transferencia não pôde ser realizada");
         }
